df3064661c3c616271ddc02fadbb2cabc69ac185
Elena <ek28150@gmail.com>
2024-08-31 22:47:40 -0700

    created functions for coffee section

--- index.js
+++ index.js
@@ -5,39 +5,94 @@ fetch(url)
   .then(data => console.log(data))
   .catch((e) => console.log(e));
 
-// drinks page //
-/* Hot coffee */
-  async function fetchHotCoffeeData() {
-    try {
-      const response = await fetch('https://api.sampleapis.com/coffee/hot');
-      
-      if (!response.ok) {
-        throw new Error('Request failed');
-      }
-      
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error('An error occurred:', error);
-    }
-  }
-  
-  fetchHotCoffeeData();
 
-/* iced coffee */
-  async function fetchIcedCoffeeData() {
-    try {
-      const response = await fetch('https://api.sampleapis.com/coffee/iced');
-      
-      if (!response.ok) {
-        throw new Error('Request failed');
-      }
-      
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error('An error occurred:', error);
+
+// Coffee section //
+
+/* Iced coffee */
+// get data about the names of iced coffee //
+async function getIcedCoffeeData() {
+  try {
+    const response = await fetch('https://api.sampleapis.com/coffee/iced');
+    if (!response.ok) {
+    throw new Error('Request failed');
     }
+
+    const data = await response.json();
+    data.forEach (coffee => {
+    const kinds = `<div> ${coffee.title} </div>`;
+    document.querySelector ('.coffee-name').insertAdjacentHTML(`beforeend`, kinds);
+    });
+
+  } catch (error) {
+    console.error(error);
   }
+}
+
+getIcedCoffeeData()
+
+// get data of iced coffee images//
+async function getIcedCoffeeImages() {
+  try {
+    const response = await fetch('https://api.sampleapis.com/coffee/iced');
+    if (!response.ok) {
+      throw new Error('Request failed');
+    }
   
-  fetchIcedCoffeeData();
+    const data = await response.json();
+    data.forEach (coffee => {
+    const photo = `<div> 
+    <img src="${coffee.image}" alt="${coffee.name}" style="width: 200px; height: auto;"></div>`;
+    document.querySelector ('.coffee-image').insertAdjacentHTML(`beforeend`, photo);
+    });
+  
+  } catch (error) {
+    console.error(error);
+  }
+}
+  
+getIcedCoffeeImages()
+
+/* Hot coffee */
+// get data about the names of hot coffee //
+async function getHotCoffeeData() {
+  try {
+    const response = await fetch('https://api.sampleapis.com/coffee/hot');
+    if (!response.ok) {
+    throw new Error('Request failed');
+    }
+
+    const data = await response.json();
+    data.forEach (coffee => {
+    const kinds = `<div> ${coffee.title} </div>`;
+    document.querySelector ('.coffee-name').insertAdjacentHTML(`beforeend`, kinds);
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getHotCoffeeData()
+
+// get data of hot coffee images//
+async function getHotCoffeeImages() {
+  try {
+    const response = await fetch('https://api.sampleapis.com/coffee/hot');
+    if (!response.ok) {
+      throw new Error('Request failed');
+    }
+  
+    const data = await response.json();
+    data.forEach (coffee => {
+    const photo = `<div> 
+    <img src="${coffee.image}" alt="${coffee.name}" style="width: 200px; height: auto;"></div>`;
+    document.querySelector ('.coffee-image').insertAdjacentHTML(`beforeend`, photo);
+    });
+  
+  } catch (error) {
+    console.error(error);
+  }
+}
+  
+getHotCoffeeImages()
